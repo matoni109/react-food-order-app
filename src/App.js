7268e85e0ba92a75b03a47acaa09f88326c499d1
Chris Wade <chris@wadespace.net>
2021-04-26 17:17:48 +1000

    more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import React from "react";
+import Header from "./components/Layout/Header";
+
+const App = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Image, CloudinaryContext, Transformation } from "cloudinary-react";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <CloudinaryContext cloudName="dr2satryk">
+          <Image
+            cloudName="dr2satryk"
+            publicId="meals_dy2ur7.jpg"
+            alt="table full
+              of food"
+          >
+            <Transformation effect="improve" quality="auto" crop="scale" />
+          </Image>
+        </CloudinaryContext>
+      </div>
+    </>
+  );
+};
+
+export default Header;
