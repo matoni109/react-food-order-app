8d91159752a0009ebd7b66e771e14472694c3156
Chris Wade <chris@wadespace.net>
2021-04-27 17:34:42 +1000

    done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = [{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(
+    (item) => {
+      <li>{item.name}</li>;
+    }
+  );
+
+  return (
+    <div>
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>36.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}></button>
+      </div>
+    </div>
+  );
+};
+export default Cart;
